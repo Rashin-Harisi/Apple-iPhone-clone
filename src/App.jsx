ae89655e73181754fa71dd69ad1d4d6263cc4623
Rashin-Harisi <rashin.harisi@gmail.com>
2024-07-01 21:08:07 +0200

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import Features from "./components/Features"
+import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
+import HowItWorks from "./components/HowItWorks"
+import Model from "./components/Model"
 import Navbar from "./components/Navbar"
 
 
@@ -10,7 +14,11 @@ const App=()=> {
     <main className="bg-black">
       <Navbar />
       <Hero />
-      <Highlights/>      
+      <Highlights/> 
+      <Model/>
+      <Features/> 
+      <HowItWorks/>  
+      <Footer/>
     </main>
   )
 }
---
+++ src/components/ModelView.jsx
@@ -0,0 +1,65 @@
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
+import Lights from "./Light";
+import { Suspense } from "react";
+import Iphone from "./IPhone";
+import * as THREE from "three";
+import Loader from "./Loader";
+
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  item,
+  size,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${index === 2 ? "right-[-100%]" : ""}`}
+    >
+      {/*Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      {/*Camera*/}
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      {/*Lights */}
+      <Lights />
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+      {/* onENd => Fires when an interaction has finished.
+
+        getAzimuthalAngle => to get current horizontal rotation in radians
+       
+        target =>The focus point of the controls, the .object orbits around this. 
+        It can be updated manually at any point to change the focus of the controls.
+      */}
+
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? "small" : "large"`}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<Loader/>}>
+          <Iphone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
+    </View>
+  );
+};
+
+export default ModelView;
